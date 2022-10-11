79a1c0c12c27e8c2828f069e9852bc89040a2ae5
Byeong Gwan Seo <seo.jp.work@gmail.com>
2022-10-11 15:14:05 +0900

    TabBar added

--- DailyWeather/DailyWeather/View/Main/ContentView.swift
+++ DailyWeather/DailyWeather/View/Main/ContentView.swift
@@ -10,41 +10,48 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var selectedMenu = 1
-    @State private var selectedDate = Date()
     
     var body: some View {
-        VStack(spacing: 30) {
-            Picker(selection: $selectedMenu, label: Text("Picker"), content: {
-                Text("Today").tag(1)
-                    .font(.largeTitle)
-                    .font(.largeTitle)
-                Text("Forecast").tag(2)
-            })
-            .pickerStyle(SegmentedPickerStyle())
-            .frame(width: 250)
-            
-            
-            if selectedMenu == 1 {
-                Text("Seoul")
-                    .font(.largeTitle)
-                Image(systemName: "sun.max.fill")
-                    .renderingMode(.original)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 200, height: 200)
-                HStack(alignment: .center, spacing: 50) {
-                    Text("Max: 27")
-                        .font(.title3)
-                    Text("Min: 14")
-                        .font(.title3)
+        TabView{
+            VStack(spacing: 30) {
+                Picker(selection: $selectedMenu, label: Text("Picker"), content: {
+                    Text("Today").tag(1)
+                        .font(.largeTitle)
+                        .font(.largeTitle)
+                    Text("Forecast").tag(2)
+                })
+                .pickerStyle(.segmented)
+                .frame(width: 250)
+                
+                Spacer(minLength: 10)
+                
+                if selectedMenu == 1 {
+                    Text("Seoul")
+                        .font(.largeTitle)
+                    Image(systemName: "sun.max.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 200, height: 200)
+                    HStack(alignment: .center, spacing: 50) {
+                        Text("Max: 27")
+                            .font(.title3)
+                        Text("Min: 14")
+                            .font(.title3)
+                    }
+                } else {
+                    Text("Forecast Here")
                 }
-            } else {
-                Text("Forecast Here")
+                Spacer()
             }
-            
-            Spacer()
+            .tabItem{
+                Text("Main")
+            }
+            Text("Search")
+                .tabItem{
+                    Text("Search")
+                }
         }
-        
     }
 }
 
