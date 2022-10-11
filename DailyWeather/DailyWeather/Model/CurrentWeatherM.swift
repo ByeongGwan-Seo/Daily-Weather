acdb3ff35ce0dfc582db6f571b8e2668f3d9f174
Byeong Gwan Seo <seo.jp.work@gmail.com>
2022-10-11 15:24:41 +0900

    Create CurrentWeatherM.swift

---
+++ DailyWeather/DailyWeather/Model/CurrentWeatherM.swift
@@ -0,0 +1,39 @@
+//
+//  CurrentWeather.swift
+//  DailyWeather
+//
+//  Created by seobyeonggwan on 2022/10/06.
+//
+
+import Foundation
+
+struct WeatherInformation: Codable {
+    let weather: [Weather]
+    let temp: Temp
+    let name: String
+    
+    enum CodingKeys: String, CodingKey {
+        case weather
+        case temp = "main"
+        case name
+    }
+}
+
+struct Weather: Codable {
+    let id: Int
+    let main: String
+    let description: String
+    let icon: String
+}
+
+struct Temp: Codable {
+    let temp: Double
+    let minTemp: Double
+    let maxTemp: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case minTemp = "temp_min"
+        case maxTemp = "temp_max"
+    }
+}
