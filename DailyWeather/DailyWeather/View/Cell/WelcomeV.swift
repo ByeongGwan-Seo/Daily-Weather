c5b7be05fcf2dba280820d5443a02b67a2c44a3c
Byeong Gwan Seo <seo.jp.work@gmail.com>
2022-10-26 03:39:31 +0900

    검색창 생성

--- DailyWeather/DailyWeather/View/Cell/WelcomeV.swift
+++ DailyWeather/DailyWeather/View/Cell/WelcomeV.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 struct WelcomeV: View {
     
+    @State var searchText = ""
+    @State var searching = false
+    
     var body: some View {
         VStack {
-            VStack {
+            VStack (spacing: 30){
+                
                 Text("Daily Weatherへようこそ")
                     .bold().font(.title)
                 
@@ -21,11 +25,22 @@ struct WelcomeV: View {
                 
                 Text("※ 都市名は英語でご記入お願い致します")
                     .font(.caption)
+            
+                HStack{
+                    SearchBar(searchText: $searchText, searching: $searching)
+                    
+                    Button{
+                        
+                    } label: {
+                        Text("確認")
+                    }
+                    .padding()
+                    .background(.white)
+                    .cornerRadius(13)
+                }
             }
             .multilineTextAlignment(.center)
             .padding()
-            
-            
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
