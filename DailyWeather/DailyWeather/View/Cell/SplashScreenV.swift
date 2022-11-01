951b331c87b6807888100d919ef6a2dd38a22572
Byeong Gwan Seo <seo.jp.work@gmail.com>
2022-11-01 10:50:53 +0900

    SplashScreen

---
+++ DailyWeather/DailyWeather/View/Cell/SplashScreenV.swift
@@ -0,0 +1,20 @@
+//
+//  SplashScreenV.swift
+//  DailyWeather
+//
+//  Created by seobyeonggwan on 2022/11/01.
+//
+
+import SwiftUI
+
+struct SplashScreenV: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SplashScreenV_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashScreenV()
+    }
+}
