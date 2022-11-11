ab9d897257ecba1e9e4d140b6a62967594e53b39
Byeong Gwan Seo <seo.jp.work@gmail.com>
2022-11-11 17:30:09 +0900

    mainUI 리모델링중
    머리가 나쁘면 손발이 고생한다를 실천중입니다

--- DailyWeather/DailyWeather/View/Main/MainView2.swift
+++ DailyWeather/DailyWeather/View/Main/MainView2.swift
@@ -9,6 +9,11 @@ import SwiftUI
 
 struct MainView2: View {
     
+    private var twoRowGrid = [
+        GridItem(.flexible(minimum: 100, maximum: 200)),
+        GridItem(.flexible(minimum: 100, maximum: 200)),
+    ]
+    
     @State private var selectedMenu: Int = 1
     
     var body: some View {
@@ -18,48 +23,185 @@ struct MainView2: View {
                                startPoint: .top,
                                endPoint: .bottomTrailing)
                 .ignoresSafeArea()
+                Rectangle()
+                    .foregroundColor(Color.white.opacity(0.3))
+                    .padding(.bottom)
+                    .edgesIgnoringSafeArea(.top)
                 ZStack {
-                    Rectangle()
+                    VStack(spacing: 40) {
+                        Picker(selection: $selectedMenu) {
+                            Text("Today").tag(1)
+                                .font(.largeTitle)
+                            Text("Forecast").tag(2)
+                        } label: {
+                            Text("oiu")
+                        }
+                        .pickerStyle(.segmented)
+                        .frame(width: 300)
+                        ScrollView() {
+                            VStack(spacing: 80) {
+                                HStack {
+                                    Spacer()
+                                    Text("Tokyo")
+                                        .fontWeight(.heavy)
+                                    Spacer()
+                                    Text("Min")
+                                    Text("/")
+                                        .fontWeight(.bold)
+                                    Text("Max")
+                                    Spacer()
+                                }
+                                .frame(maxWidth: .infinity, alignment: .center)
+                                .foregroundColor(.primary)
+                                .font(.title)
+                                
+                                Image(systemName: "sun.max.fill")
+                                    .renderingMode(.original)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 200, height: 200)
+                                
+                                VStack(spacing: 20) {
+                                    HStack(alignment: .center, spacing: 30) {
+                                        HStack {
+                                            Image(systemName: "figure.stand")
+                                                .resizable()
+                                                .scaledToFit()
+                                                .frame(width: 50, height: 50)
+                                            VStack {
+                                                Text("Feels like")
+                                                Text("134")
+                                            }
+                                        }
+                                        .padding()
+                                        .background()
+                                        .cornerRadius(20)
+                                        .frame(width: 160)
+                                        
+                                        HStack {
+                                            Image(systemName: "eye.circle")
+                                                .resizable()
+                                                .scaledToFit()
+                                                .frame(width: 50, height: 50)
+                                            VStack {
+                                                Text("Visibility")
+                                                Text("134")
+                                            }
+                                        }
+                                        .padding()
+                                        .background()
+                                        .cornerRadius(20)
+                                        .frame(width: 160)
+                                    }
+                                    
+                                    HStack(alignment: .center, spacing: 30) {
+                                        HStack {
+                                            Image(systemName: "figure.stand")
+                                                .resizable()
+                                                .scaledToFit()
+                                                .frame(width: 50, height: 50)
+                                            VStack {
+                                                Text("Feels like")
+                                                Text("134")
+                                            }
+                                        }
+                                        .padding()
+                                        .background()
+                                        .cornerRadius(20)
+                                        .frame(width: 160)
+                           
+                                        HStack {
+                                            Image(systemName: "eye.circle")
+                                                .resizable()
+                                                .scaledToFit()
+                                                .frame(width: 50, height: 50)
+                                            VStack {
+                                                Text("Visibility")
+                                                Text("134")
+                                            }
+                                        }
+                                        .padding()
+                                        .background()
+                                        .cornerRadius(20)
+                                        .frame(width: 160)
+                                    }
+                                    HStack(alignment: .center, spacing: 30) {
+                                        HStack {
+                                            Image(systemName: "figure.stand")
+                                                .resizable()
+                                                .scaledToFit()
+                                                .frame(width: 50, height: 50)
+                                            VStack {
+                                                Text("Feels like")
+                                                Text("134")
+                                            }
+                                        }
+                                        .padding()
+                                        .background()
+                                        .cornerRadius(20)
+                                        .frame(width: 160)
+                                        
+                                        HStack {
+                                            Image(systemName: "eye.circle")
+                                                .resizable()
+                                                .scaledToFit()
+                                                .frame(width: 50, height: 50)
+                                            VStack {
+                                                Text("Visibility")
+                                                Text("134")
+                                            }
+                                        }
+                                        .padding()
+                                        .background()
+                                        .cornerRadius(20)
+                                        .frame(width: 160)
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
-                .foregroundColor(Color.white.opacity(0.3))
-                .edgesIgnoringSafeArea(.top)
-                .padding(.bottom)
             }
             .tabItem {
                 Image(systemName: "globe")
                 Text("MAIN")
             }
             Text("Search")
-            .tabItem {
-                Image(systemName: "magnifyingglass")
-                Text("SEARCH")
-            }
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("SEARCH")
+                }
         }
-        
     }
     
-//    func weatherImage(weather: String) -> Image {
-//        switch weather {
-//        case "Clear":
-//            return Image(systemName: "sun.max.fill")
-//        case "Clouds":
-//            return Image(systemName: "cloud.fill")
-//        case "Rain":
-//            return Image(systemName: "cloud.rain.fill")
-//        case "Snow":
-//            return Image(systemName: "cloud.snow.fill")
-//        case "Extreme":
-//            return Image(systemName: "cloud.bolt.fill")
-//        default:
-//            return Image(systemName: "sun.max.fill")
-//        }
-//    }
+    //    func weatherImage(weather: String) -> Image {
+    //        switch weather {
+    //        case "Clear":
+    //            return Image(systemName: "sun.max.fill")
+    //        case "Clouds":
+    //            return Image(systemName: "cloud.fill")
+    //        case "Rain":
+    //            return Image(systemName: "cloud.rain.fill")
+    //        case "Snow":
+    //            return Image(systemName: "cloud.snow.fill")
+    //        case "Extreme":
+    //            return Image(systemName: "cloud.bolt.fill")
+    //        default:
+    //            return Image(systemName: "sun.max.fill")
+    //        }
+    //    }
+    
 }
 
 struct MainView2_Previews: PreviewProvider {
     static var previews: some View {
-        MainView2()
-            .preferredColorScheme(.dark)
+        Group {
+            MainView2()
+                .preferredColorScheme(.dark)
+                .previewInterfaceOrientation(.portrait)
+            MainView2()
+                .previewInterfaceOrientation(.portrait)
+        }
     }
 }
 
