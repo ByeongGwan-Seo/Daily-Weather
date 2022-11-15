334f94de848e854550d1f0f5991931803cc31ec8
Byeong Gwan Seo <seo.jp.work@gmail.com>
2022-11-15 13:02:34 +0900

    list background 및 frame 미세 조정

--- DailyWeather/DailyWeather/View/Main/SearchV2.swift
+++ DailyWeather/DailyWeather/View/Main/SearchV2.swift
@@ -11,28 +11,35 @@ struct SearchV2: View {
     
     @EnvironmentObject var vm: CurrentWeatherViewModel
     @State var searching = false
+    @State var searchText = ""
     
     var body: some View {
-        SearchBar(searchText: $vm.searchText, searching: $searching)
         
-        Text("検索履歴")
-            .font(.largeTitle)
+        SearchBar(searchText: $vm.searchText, searching: $searching)
+
+        VStack {
+            Text("検索履歴")
+                .font(.largeTitle)
             .fontWeight(.bold)
-        List {
-            Text("This is searchV2")
-            Text("예정 기능 -> 1) searchV2에 입력한 값을 mainV2로 2)사용자의 검색 히스토리 저장")
-            Text("살려줘 메우")
+            List {
+                Text("This is searchV2")
+                Text("예정 기능 -> 1) searchV2에 입력한 값을 mainV2로 2)사용자의 검색 히스토리 저장")
+                Text("살려줘 메우")
+                Text("살려줘 메우")
+            }
+            .frame(width: 250, height: 400)
+            .background()
+            .cornerRadius(30)
+            .listStyle(.plain)
+            .foregroundColor(Color.primary)
         }
-        .listStyle(.plain)
-        .background(.ultraThinMaterial)
-        .background(Color.primary)
-        .foregroundColor(Color.primary)
     }
 }
 
 struct SearchV2_Previews: PreviewProvider {
     static var previews: some View {
         SearchV2()
+            .preferredColorScheme(.dark)
             .environmentObject(dev.currentWeatherVM)
     }
 }
