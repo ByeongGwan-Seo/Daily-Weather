9b7c08d5f092b00c82c039f92ac5b0faf9395944
Byeong Gwan Seo <seo.jp.work@gmail.com>
2022-11-15 15:12:10 +0900

    안쓰는 애들 주석처리

--- DailyWeather/DailyWeather/View/Cell/SearchV.swift
+++ DailyWeather/DailyWeather/View/Cell/SearchV.swift
@@ -7,53 +7,53 @@
 
 import SwiftUI
 
-struct SearchV: View {
+//struct SearchV: View {
+//
+//    @EnvironmentObject var vm: CurrentWeatherViewModel
+//    @State var searching = false
+//
+//    let cities = ["Seoul", "Tokyo", "Osaka", "Busan", "Kyoto", "Toronto", "Osan"]
+//
+//    var body: some View {
+//        NavigationView {
+//            let filteredCities = cities.filter { $0.hasPrefix(vm.searchText) || vm.searchText == "" }
+//
+//            VStack(alignment: .leading) {
+//                SearchBar(searchText: $vm.searchText, searching: $searching)
+//                List(filteredCities, id:\.self) {city in ZStack {
+//                    NavigationLink {
+//                        CurrentWeatherV()
+//                    } label: {
+//                        EmptyView()
+//                    }
+//                    Text(city)
+//                }
+//                .frame(height: 60)
+//                }
+//                    .listStyle(GroupedListStyle())
+//                    .navigationTitle(searching ? "Searching" : "Search city")
+//                    .toolbar {
+//                        if searching {
+//                            Button("Cancel") {
+//                                vm.searchText = ""
+//                                withAnimation {
+//                                    searching = false
+//                                    UIApplication.shared.dismissKeyboard()
+//                                }
+//                            }
+//                        }
+//                    }
+//            }
+//        }
+//    }
+//}
 
-    @EnvironmentObject var vm: CurrentWeatherViewModel
-    @State var searching = false
-    
-    let cities = ["Seoul", "Tokyo", "Osaka", "Busan", "Kyoto", "Toronto", "Osan"]
 
-    var body: some View {
-        NavigationView {
-            let filteredCities = cities.filter { $0.hasPrefix(vm.searchText) || vm.searchText == "" }
-            
-            VStack(alignment: .leading) {
-                SearchBar(searchText: $vm.searchText, searching: $searching)
-                List(filteredCities, id:\.self) {city in ZStack {
-                    NavigationLink {
-                        CurrentWeatherV()
-                    } label: {
-                        EmptyView()
-                    }
-                    Text(city)
-                }
-                .frame(height: 60)
-                }
-                    .listStyle(GroupedListStyle())
-                    .navigationTitle(searching ? "Searching" : "Search city")
-                    .toolbar {
-                        if searching {
-                            Button("Cancel") {
-                                vm.searchText = ""
-                                withAnimation {
-                                    searching = false
-                                    UIApplication.shared.dismissKeyboard()
-                                }
-                            }
-                        }
-                    }
-            }
-        }
-    }
-}
-
-
-struct SearchV_Previews: PreviewProvider {
-    static var previews: some View {
-        SearchV()
-            .environmentObject(dev.currentWeatherVM)
-    }
-}
+//struct SearchV_Previews: PreviewProvider {
+//    static var previews: some View {
+//        SearchV()
+//            .environmentObject(dev.currentWeatherVM)
+//    }
+//}
 
 
