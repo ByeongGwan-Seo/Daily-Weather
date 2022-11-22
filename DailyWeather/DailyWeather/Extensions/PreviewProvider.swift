ba41f1ff88e740a64644bd4e09506c8d729c6dca
Byeong Gwan Seo <seo.jp.work@gmail.com>
2022-11-22 18:31:04 +0900

    ForecastV 기본 UI 완성

--- DailyWeather/DailyWeather/Extensions/PreviewProvider.swift
+++ DailyWeather/DailyWeather/Extensions/PreviewProvider.swift
@@ -21,7 +21,7 @@ class DeveloperPreview {
     private init() { }
     
     let currentWeatherVM = CurrentWeatherViewModel(cityName: "Seoul")
-    
+    let forecastWeatherVM = ForeCastViewModel(cityName: "tokyo")
     let weatherInfo = WeatherInformation(coord: Coord1(lon: 126.9778, lat: 37.5683), weather: [Weather(id: 803, main: "Clouds", weatherDescription: "broken clouds", icon: "04d")], base: "stations", main: Main(temp: 283.81, feelsLike: 282.16, tempMin: 283.81, tempMax: 288.84, pressure: 1027, humidity: 47), visibility: 10000, wind: Wind1(speed: 1.39, deg: 57), clouds: Clouds1(all: 56), dt: 1665619509, sys: Sys1(type: 1, id: 5509, country: "KR", sunrise: 1665610659, sunset: 1665651535), timezone: 32400, id: 1835848, name: "Seoul", cod: 200)
 }
 
--- DailyWeather/DailyWeather/View/Main/ForecastVRow.swift
+++ DailyWeather/DailyWeather/View/Main/ForecastVRow.swift
@@ -13,23 +13,66 @@ struct ForecastVRow: View {
     
     
     var body: some View {
-        VStack {
-            if let forecastWeather = vm.forecast {
-                
-                let city = forecastWeather.city?.name ?? "none"
-                Text(vm.getMaxTempCelcius())
-                Text(vm.getMinTempCelcius())
-                Text(city)
-                
-            }
+        ScrollView {
+            rowSection
+            rowSection
+            rowSection
+            rowSection
+            rowSection
         }
     }
 }
 
 struct ForecastVRow_Previews: PreviewProvider {
+    
     static var previews: some View {
         ForecastVRow()
-            .previewLayout(.fixed(width: 390, height: 80))
-            .environmentObject(ForeCastViewModel())
+            .environmentObject(dev.forecastWeatherVM)
+    }
+}
+
+extension ForecastVRow {
+    private var rowSection: some View {
+        VStack {
+            HStack {
+                Spacer()
+                Text("대충날짜")
+                    .font(.headline)
+                Spacer()
+                Image(systemName: "sun.max.fill")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                Spacer()
+                Text(vm.getMinTempCelcius() + "°")
+                Text("/")
+                Text(vm.getMaxTempCelcius() + "°")
+                Spacer()
+            }
+            .padding(.horizontal)
+            Divider()
+            HStack {
+                VStack(spacing: 10) {
+                    Text("湿度")
+                    Text("sidou")
+                }
+                Spacer()
+                VStack(spacing: 10) {
+                    Text("体感温度")
+                    Text("몇도")
+                }
+                Spacer()
+                VStack(spacing: 10) {
+                    Text("風速")
+                    Text("10.2m/s")
+                }
+            }
+            .padding(.horizontal, 20)
+        }
+        .frame(height: 140)
+        .frame(maxWidth: .infinity)
+        .background(LinearGradient(colors:[ Color(#colorLiteral(red: 0, green: 0.649317205, blue: 0.933945477, alpha: 1)), Color(#colorLiteral(red: 0.3848685026, green: 0.8256501555, blue: 0.7817828059, alpha: 1))], startPoint: .topLeading, endPoint: .bottomTrailing))
+        .cornerRadius(30)
+        .padding()
+        .shadow(color: Color.black, radius: 5, x: 0, y: 5)
     }
 }
